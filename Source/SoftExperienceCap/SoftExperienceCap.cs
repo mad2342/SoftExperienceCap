cf189669df349b377aa88b19879909c5248e949d
Mad <github@typ03.de>
2019-02-01 16:08:06 +0100

    fix logic bug

--- Source/SoftExperienceCap/SoftExperienceCap.cs
+++ Source/SoftExperienceCap/SoftExperienceCap.cs
@@ -5,6 +5,10 @@ using BattleTech;
 using BattleTech.UI;
 using TMPro;
 using Newtonsoft.Json;
+using UnityEngine;
+using HBS;
+
+
 
 namespace SoftExperienceCap
 {
@@ -95,8 +99,8 @@ namespace SoftExperienceCap
         }
     }
 
-    
-    
+
+
     // Info
     [HarmonyPatch(typeof(SGBarracksWidget), "OnPilotSelected")]
     public static class SGBarracksWidget_OnPilotSelected_Patch
@@ -123,6 +127,26 @@ namespace SoftExperienceCap
         }
     }
 
+    /*
+     * BEN: This is the real shit. For now i'm patching into this ONLY to check that my data injection via UI-Patches actually get called AFTER this.
+     */
+    [HarmonyPatch(typeof(Contract), "CompleteContract")]
+    public static class Contract_CompleteContract_Patch
+    {
+        public static void Postfix(Contract __instance, MissionResult result, bool isGoodFaithEffort)
+        {
+            try
+            {
+                // Same for ALL pilots
+                int ExperienceEarned = (int)AccessTools.Property(typeof(Contract), "ExperienceEarned").GetValue(__instance, null);
+                Logger.LogLine("[Contract_CompleteContract_POSTFIX] ExperienceEarned: " + ExperienceEarned);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+            }
+        }
+    }
 
 
     [HarmonyPatch(typeof(AAR_UnitStatusWidget), "FillInData")]
@@ -132,6 +156,9 @@ namespace SoftExperienceCap
         {
             try
             {
+                // NOTE that all vanilla getters already include xpEarned in their values! This is already set and done at this point.
+                // For correct calculations the param "xpEarned" must be substracted first!
+
                 Pilot p = ___UnitData.pilot;
                 PilotDef pDef = p.pilotDef;
 
@@ -144,7 +171,10 @@ namespace SoftExperienceCap
                 int AbsoluteExperienceSpent = Utilities.GetAbsoluteExperienceSpent(pDef, ___simState);
                 Logger.LogLine("[AAR_UnitStatusWidget_FillInData_PREFIX] (" + p.Name + ") AbsoluteExperienceSpent: " + AbsoluteExperienceSpent);
 
-                int SurplusXP = p.TotalXP - pDef.ExperienceSpent; Logger.LogLine("[AAR_UnitStatusWidget_FillInData_PREFIX] " + p.Name + "s SurplusXP: " + SurplusXP);
+                int SurplusXP = p.TotalXP - pDef.ExperienceSpent - xpEarned;
+                Logger.LogLine("[AAR_UnitStatusWidget_FillInData_PREFIX] (" + p.Name + ") SurplusXP: " + SurplusXP + "(already substracted xpEarned of: " + xpEarned + ")");
+                Logger.LogLine("[AAR_UnitStatusWidget_FillInData_PREFIX] (" + p.Name + ") UnspentXP: " + p.UnspentXP + "(still includes uncorrected xpEarned of: " + xpEarned + ")");
+
                 int AbsoluteExperience = AbsoluteExperienceSpent + SurplusXP;
                 Logger.LogLine("[AAR_UnitStatusWidget_FillInData_PREFIX] (" + p.Name + ") AbsoluteExperience: " + AbsoluteExperience);
 
@@ -159,13 +189,13 @@ namespace SoftExperienceCap
                 if (AbsoluteExperience >= xpSoftCap)
                 {
                     xpTemp = xpMinimum;
-                    Logger.LogLine("[AAR_UnitStatusWidget_FillInData_PREFIX] (" + p.Name + ") experience is above cap. Gaining only minimum XP.");
+                    Logger.LogLine("[AAR_UnitStatusWidget_FillInData_PREFIX] (" + p.Name + ") Experience is above cap. Gaining only minimum XP.");
                 }
                 // Not more than XPCap + minimum XP
                 if (AbsoluteExperience < xpSoftCap && PotentialExperienceAfterMissionReward >= xpSoftCap)
                 {
                     xpTemp = (PotentialExperienceAfterMissionReward - xpSoftCap) + xpMinimum;
-                    Logger.LogLine("[AAR_UnitStatusWidget_FillInData_PREFIX] (" + p.Name + ") experience is hitting cap. Gaining less XP.");
+                    Logger.LogLine("[AAR_UnitStatusWidget_FillInData_PREFIX] (" + p.Name + ") Experience is hitting cap. Gaining less XP.");
                 }
                 // Absolutely no XP when at games hard limit?
                 if (AbsoluteExperience >= xpLimit)
@@ -182,8 +212,94 @@ namespace SoftExperienceCap
                     xpTemp += SoftExperienceCap.Settings.xpMissionOtherKilled;
                 }
 
-                // Set
+                // Set/Adjust unspent experience 
+                int unspentXP = p.UnspentXP; // At this point UnspentXP still includes the originally earned XP so unmodified xpEarned needs to be substracted first
+                int adjustedUnspentXP = unspentXP - xpEarned + xpTemp;
+                Logger.LogLine("[AAR_UnitStatusWidget_FillInData_PREFIX] (" + p.Name + ") adjustedUnspentXP: " + adjustedUnspentXP);
+                p.StatCollection.Set<int>("ExperienceUnspent", adjustedUnspentXP);
+                pDef.SetUnspentExperience(adjustedUnspentXP);
+
+                // Modify for UI
                 xpEarned = xpTemp;
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+            }
+        }
+        public static void Postfix(AAR_UnitStatusWidget __instance, UnitResult ___UnitData)
+        {
+            try
+            {
+                Pilot p = ___UnitData.pilot;
+
+                Logger.LogLine("[AAR_UnitStatusWidget_FillInData_POSTFIX] CHECK (" + p.Name + ") UnspentXP: " + p.UnspentXP);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(AAR_UnitStatusWidget), "FillInPilotData")]
+    public static class AAR_UnitStatusWidget_FillInPilotData_Patch
+    {
+        public static void Postfix(AAR_UnitStatusWidget __instance, int xpEarned, SimGameState ___simState, UnitResult ___UnitData, TextMeshProUGUI ___XPText, SGBarracksRosterSlot ___PilotWidget)
+        {
+            try
+            {
+                Pilot p = ___UnitData.pilot;
+                PilotDef pDef = p.pilotDef;
+
+                int xpSoftCap = ___simState.GetCurrentExperienceCap();
+                int AbsoluteExperienceSpent = Utilities.GetAbsoluteExperienceSpent(pDef, ___simState);
+                int UnspentXP = p.UnspentXP;
+                int AbsoluteExperience = AbsoluteExperienceSpent + UnspentXP;
+                
+                // NOTE that xpEarned is already adjusted at this point
+                Logger.LogLine("[AAR_UnitStatusWidget_FillInPilotData_POSTFIX] (" + p.Name + ") xpEarned: " + xpEarned);
+                Logger.LogLine("[AAR_UnitStatusWidget_FillInPilotData_POSTFIX] (" + p.Name + ") AbsoluteExperience: " + AbsoluteExperience);
+
+                /*
+                ___XPText.SetText("+{0}XP ({1} / {2}XP)", new object[]
+                {
+                    xpEarned,
+                    AbsoluteExperience,
+                    xpSoftCap
+                });
+                */
+
+                Color red = LazySingletonBehavior<UIManager>.Instance.UIColorRefs.red;
+                Color gold = LazySingletonBehavior<UIManager>.Instance.UIColorRefs.gold;
+                Color green = LazySingletonBehavior<UIManager>.Instance.UIColorRefs.green;
+                string htmlColorTag = "";
+                if (AbsoluteExperience > xpSoftCap)
+                {
+                    htmlColorTag = "<color=#" + ColorUtility.ToHtmlStringRGBA(red) + ">";
+                }
+                //else if ((AbsoluteExperience + xpEarned) > xpSoftCap)
+                //{
+                //    htmlColorTag = "<color=#" + ColorUtility.ToHtmlStringRGBA(gold) + ">";
+                //}
+                else
+                {
+                    htmlColorTag = "<color=#" + ColorUtility.ToHtmlStringRGBA(green) + ">";
+                }
+
+                TextMeshProUGUI callsign = (TextMeshProUGUI)AccessTools.Field(typeof(SGBarracksRosterSlot), "callsign").GetValue(___PilotWidget);
+                callsign.enableAutoSizing = false;
+                callsign.enableWordWrapping = false;
+                callsign.fontSize = callsign.fontSize + 1;
+                callsign.OverflowMode = TextOverflowModes.Overflow;
+
+                callsign.SetText("{0} " + htmlColorTag + "({1} / {2}XP)</color>", new object[]
+                {
+                    p.Callsign,
+                    AbsoluteExperience,
+                    xpSoftCap
+                });
+
             }
             catch (Exception e)
             {
